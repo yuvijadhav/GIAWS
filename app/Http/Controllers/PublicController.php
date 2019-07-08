d102e3e266bdbdd6f834a0ab3cfb75c2c8ffdd98
root <root@ip-172-31-87-219.ec2.internal>
2019-07-08 06:44:14 +0000

    comm

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -380,12 +380,12 @@ class PublicController extends Controller {
 
         if ($url != "null") {
 
-            $reportURLdata = DB::table('reportsurl')->where('url', '%' . $url . '%')->first();
+            $reportURLdata = DB::table('reportsurl')->where('url', $url)->first();
             if (($reportURLdata) != NULL) {
                 $report_id = $reportURLdata->report_id;
                 $report = Report::where('status', 1)->where("report_id", $report_id)->with("publisher")->with("subCategory")->with("region")->first();
             } else {
-                $report = Report::where('status', 1)->where("url", '%' . $url . '%')->with("publisher")->with("subCategory")->with("region")->first();
+                $report = Report::where('status', 1)->where("url", $url)->with("publisher")->with("subCategory")->with("region")->first();
             }
 
             $relatedReports = Report::where('status', 1)->where("sub_category_id", $report->sub_category_id)->where("report_id", "!=", $report->report_id)->with("publisher")->with("subCategory")->orderBy('report_id', 'desc')->take(2)->get();
@@ -439,20 +439,23 @@ class PublicController extends Controller {
         $sub_categories = SubCategory::all();
         if ($url != "null") {
 
-            $reportURLdata = DB::table('reportsurl')->where('url', '%' . $url . '%')->first();
+            $reportURLdata = DB::table('reportsurl')->where('url', $url)->first();
+//            dd($reportURLdata);
             if (($reportURLdata) != NULL) {
                 $report_id = $reportURLdata->report_id;
                 $report = Report::where('status', 1)->where("report_id", $report_id)->with("publisher")->with("subCategory")->with("region")->first();
                 $relatedReports = Report::where('status', 1)->where("sub_category_id", $report->sub_category_id)->where("report_id", "!=", $report->report_id)->with("publisher")->with("subCategory")->orderBy('report_id', 'desc')->take(2)->get();
                 return view('report.reportDetails')->with('report', $report)->with('relatedReports', $relatedReports)->with('sub_categories', $sub_categories);
             } else {
-                $report = FrontReport::where('status', 1)->where("url", '%' . $url . '%')->with("publisher")->with("subCategory")->with("region")->with("reportdetails")->first();
+                $report = FrontReport::where('status', 1)->where("url", $url)->with("publisher")->with("subCategory")->with("region")->with("reportdetails")->first();
                 if (($report) != NULL) {
-                    $relatedReports = Report::where('status', 1)->where("sub_category_id", $report->sub_category_id)->where("report_id", "!=", $report->report_id)->with("publisher")->with("subCategory")->orderBy('report_id', 'desc')->take(2)->get();
+//                    dd($report->sub_category_id);
+//                    $relatedReports = Report::where('status', 1)->where("sub_category_id", $report->sub_category_id)->where("report_id", "!=", $report->report_id)->with("publisher")->with("subCategory")->orderBy('report_id', 'desc')->take(2)->get();
+                    $relatedReports = Report::where('status', 1)->where("sub_category_id", $report->sub_category_id)->with("publisher")->with("subCategory")->orderBy('report_id', 'desc')->take(2)->get();
+//                    $relatedReports = array();
                     return view('report.reportDetailsData')->with('report', $report)->with('relatedReports', $relatedReports)->with('sub_categories', $sub_categories);
                 } else {
-
-                    $report = Report::where('status', 1)->where("url", '%' . $url . '%')->with("publisher")->with("subCategory")->with("region")->first();
+                    $report = Report::where('status', 1)->where("url", $url)->with("publisher")->with("subCategory")->with("region")->first();
                     if (($report) != NULL) {
                         $relatedReports = Report::where('status', 1)->where("sub_category_id", $report->sub_category_id)->where("report_id", "!=", $report->report_id)->with("publisher")->with("subCategory")->orderBy('report_id', 'desc')->take(2)->get();
                         return view('report.reportDetails')->with('report', $report)->with('relatedReports', $relatedReports)->with('sub_categories', $sub_categories);
